12feffa67e2d853822fb6035823eb1b5e536e987
hassankhalid10 <hassan.baloch87@gmail.com>
2022-07-07 12:35:32 +0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/aboutus', function(req, res, next) {
+  res.render('about', {page:'About Us', menuId:'about'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact Us', menuId:'contact'});
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('portfolio', {page:'Portfolio', menuId:'Portfolio'});
+});
+
+module.exports = router;
